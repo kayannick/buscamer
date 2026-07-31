34ffe2b2c637fe49810fb4715b3a434cecf022c0
kayannick <kamgangyannick14@gmail.com>
2026-07-31 02:10:49 +0200

    whitenoise

--- backend/buscam_project/settings.py
+++ backend/buscam_project/settings.py
@@ -134,6 +134,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Sert les fichiers statiques en prod    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
